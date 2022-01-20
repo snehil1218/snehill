18890b9c9f2c593cf5df63757ddc2024591c2f37
snehil1218 <98089086+snehil1218@users.noreply.github.com>
2022-01-20 15:51:12 +0530

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int op,x,y;
+  cout<<"press 1 for addition,2 for subtraction and 3 for multiplication\n";
+  cin>>op;
+  cout<<"enter two numbers\n";
+  cin>>x>>y;
+
+  if(op==1)
+  cout<<"sum is "<<x+y;
+  else if(op==2)
+  cout<<"difference is "<<x-y;
+  else if(op==3)
+  cout<<"product is "<<x*y;
+  else
+  cout<<"enter a valid choice";
+
+  return 0;
+  }
