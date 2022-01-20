c1c42ffe4e55a9f4671875ec9da65ccdcdc29036
snehil1218 <98089086+snehil1218@users.noreply.github.com>
2022-01-20 16:13:18 +0530

    Update cups even odd games.cpp
    we have taken a number of cups and we assumed that two users are playing this game. User who picks a last cup wins this game.

--- cups even odd games.cpp
+++ cups even odd games.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
   int a;
-  cout<<"no. of cups are\n";
+  cout<<"no. of  cups are\n";
   cin>>a;
 
   if(a%2==0)
   {
-   cout<<"opponent won\n";
+   cout<<"user 2 won\n";
    }
    else
-   cout<<"you won";
+   cout<<"user 1 won";
    return 0;
    }
