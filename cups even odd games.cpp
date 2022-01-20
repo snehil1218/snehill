d81c01f56732434ac548b6b6b2b8d092b8e6356c
snehil1218 <98089086+snehil1218@users.noreply.github.com>
2022-01-20 15:53:50 +0530

    Add files via upload

---
+++ cups even odd games.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"no. of cups are\n";
+  cin>>a;
+
+  if(a%2==0)
+  {
+   cout<<"opponent won\n";
+   }
+   else
+   cout<<"you won";
+   return 0;
+   }
